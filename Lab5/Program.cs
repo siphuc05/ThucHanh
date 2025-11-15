cf44ef9094ec759f2fd5a75c8861dbcbbca51cc9
siphuc05 <nguyensiphuc26052005@gmail.com>
2025-11-15 19:48:42 +0700

    first commit

---
+++ Lab5/Program.cs
@@ -0,0 +1,29 @@
+using Lab5.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("SchoolContext");
+builder.Services.AddDbContext<Lab4CodeFirstContext>(x => x.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Lab5/ViewComponents/MajorViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab5.Models;
+namespace Lab5.ViewComponents
+{
+    public class MajorViewComponent: ViewComponent
+    {
+        Lab4CodeFirstContext db;
+        List<Major> majors;
+        public MajorViewComponent(Lab4CodeFirstContext _context)
+        {
+            db = _context;
+            majors = db.Majors.ToList();
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderMajor",majors);
+        }
+    }
+}
